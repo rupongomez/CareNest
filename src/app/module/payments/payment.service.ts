9a802c3b50c6a575ceeb5ef496f3be47e705c0f4
RuponGomeZ <anthonyrupon0@gmail.com>
2026-08-30 18:18:04 +0600

    payment module added

--- src/app/module/payments/payment.service.ts
+++ src/app/module/payments/payment.service.ts
@@ -1,1 +1,25 @@
-const getMyPayments = async () => {};
+import { IQuery } from "../../interfaces";
+import { prisma } from "../../lib/prisma";
+import { RequestUser } from "../../middleware/checkAuth";
+import httpStatus from "http-status";
+import { AppError } from "../../utils/AppError";
+
+const getMyPayments = async (query: IQuery, user: RequestUser) => {
+  const patient = await prisma.patient.findUnique({
+    where: { userId: user.userId },
+  });
+
+  if (!patient) {
+    throw new AppError(httpStatus.NOT_FOUND, "Patient profile not found");
+  }
+};
+
+const getAllPayments = async () => {};
+
+const getSinglePayment = async () => {};
+
+export const PaymentServices = {
+  getMyPayments,
+  getAllPayments,
+  getSinglePayment,
+};
